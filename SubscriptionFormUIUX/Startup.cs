4a324aceaad4dde777767c9e5398f43d56ad2891
JINAN <JINAN@JINAN-PC>
2018-05-27 21:33:21 -0400

    Add project files.

---
+++ SubscriptionFormUIUX/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SubscriptionFormUIUX.Startup))]
+namespace SubscriptionFormUIUX
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ SubscriptionFormUIUX/Views/Home/GetSalt.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CirculationMonitor.aspnet
+{
+    public partial class GetSalt : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string ReturnSalt= "";
+           
+            if (Request["query"] != null)
+            {
+                var q = Request["query"].ToString();
+                if (q == "getmysalt")
+                {
+                    ReturnSalt = "DEEA7503-EEA7-46D3-8396-11BC701BC70C";
+                }
+            }
+
+            Response.Write(ReturnSalt);
+        }
+    }
+}
